63790df841aa250ccabc17aa5ae50f07eaff9323
Charlie <Charlie>
2017-05-21 18:00:58 -0400

    Fixed a bug in require for Linux

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 #! /usr/bin/env node
 "use strict";
+const path = require("path");
 
 if (!process.env["npm_config_argv"]) {
     console.log("This is meant to be run from within npm script. See https://github.com/charlesguse/run-script-os");
     return;
 }
 const spawn = require("child_process").spawn;
-const scripts = require('./package.json').scripts;
+
+let scripts;
+if (process.platform === "win32") {
+    scripts = require("./package.json").scripts;
+} else {
+    scripts = require(path.join(process.env.PWD, "package.json")).scripts;
+}
 
 let npmArgs = JSON.parse(process.env["npm_config_argv"]);
 let options = npmArgs.original;
@@ -14,7 +21,7 @@ if (!(options[0] === "run" || options[0] === "run-script")) {
     options.unshift("run");
 }
 let osCommand = `${options[1]}:${process.platform}`
-if (!scripts.hasOwnProperty(osCommand)) {
+if (!(osCommand in scripts)) {
     let regex = new RegExp(`^(${options[1]}):([a-zA-Z0-9-]*:)*(${process.platform})(:[a-zA-Z0-9-]*)*$`, "g")
     for (let command in scripts) {
         if (command.match(regex)) {
