f626c361db703037728da8b6ecd6eafb31a5ee8c
Charlie <Charlie>
2020-12-24 17:28:21 -0500

    Reorganized the test structure to fix the powershell scoping issue. Will probably pull the variables into setup functions if more tests with files are needed

--- tests/nix.test.bats
+++ tests/nix.test.bats
@@ -1,15 +1,13 @@
 #!node_modules/.bin/bats
 
-setup() {
+@test "It should run as expected with pre/post variables" {
   date_time=$(date -u +"%Y%m%d-%H%M%S")
   # expected="20201224-210751"
-  export expected="$NIX_OS.expected.txt"
+  expected="$NIX_OS.expected.txt"
   # expected="linux.expected.txt"
-  export actual="$NIX_OS.actual.$date_time.txt"
+  actual="$NIX_OS.actual.$date_time.txt"
   # actual="linux.actual.20201224-210751.txt"
-}
 
-@test "It should run as expected with pre/post variables" {
   run $(npm run-script test --silent > $actual)
   [ "$status" -eq 0 ]
   
--- tests/windows.test.ps1
+++ tests/windows.test.ps1
@@ -1,21 +1,21 @@
 Describe "run-script-os" -Tags "CI" {
-    $date_time = Get-Date ([datetime]::UtcNow) -UFormat "%Y%m%d-%H%M%S"
-    # expected = "20201224-210751"
-    $expected = "windows.expected.txt"
-    $actual = "windows.actual.$date_time.txt"
-    # actual = "windows.actual.20201224-210751.txt"
-
     It "should run as expected with pre/post variables" {
+        $date_time = Get-Date ([datetime]::UtcNow) -UFormat "%Y%m%d-%H%M%S"
+        # date_time = "20201224-210751"
+        $expected = "windows.expected.txt"
+        $actual = "windows.actual.$date_time.txt"
+        # actual = "windows.actual.20201224-210751.txt"
+
         npm run-script test --silent > $actual
-        $LASTEXITCODE | Should be 0
+        $LASTEXITCODE | Should -Be 0
 
         $diff = Compare-Object -ReferenceObject (Get-Content $expected) -DifferenceObject (Get-Content $actual)
         $diffCount = $diff | Measure-Object | Select-Object -ExpandProperty Count
-        $diffCount | Should be 0
+        $diffCount | Should -Be 0
     }
 
     It "should be able to error out as expected" {
         npm run-script test-error --silent
-        $LASTEXITCODE | Should be 22
+        $LASTEXITCODE | Should -Be 22
     }
 }
